6a62df685acb273454c89c6a2dbfd862af572ccb
Daniel Jaramillo <daretana90@gmail.com>
2025-04-15 21:19:59 -0600

    Adding routes to UPDATE and DELETE

--- servers.js
+++ servers.js
@@ -11,15 +11,16 @@ app.listen(PORT, () => console.log(`SERVER RUNNING ON PORT ${PORT}`));
 
 // Get users
 app.get("/api/users/", async (req, res) => {
+  console.log("GET - ", "/api/users/");
   try {
     const pool = await poolPromise;
     const result = await pool.query("SELECT * FROM [CURSO_DB].[dbo].[users]");
     // console.log(result);
-    console.log("/api/users/");
-    res.status(200).json({
-      succes: true,
-      empData: result.recordset,
-    });
+    // res.status(200).json({
+    //   succes: true,
+    //   empData: result.recordset,
+    // });
+    res.status(200).json(result.recordset);
   } catch (error) {
     console.log("Error", error);
     res.status(500).json({
@@ -30,7 +31,8 @@ app.get("/api/users/", async (req, res) => {
 });
 
 // Get user by id
-app.get("/api/users/:id", async (req, res) => {
+app.get("/api/user/:id", async (req, res) => {
+  console.log("GET - ", "/api/user/1");
   try {
     const { id } = req.params;
 
@@ -53,14 +55,72 @@ app.get("/api/users/:id", async (req, res) => {
         message: "User not found",
       });
 
-    res.status(200).json({
-      succes: true,
-      empData: result.recordset[0],
-    });
+    res.status(200).json(result.recordset[0]);
   } catch (error) {
     res.status(500).json({
       succes: false,
-      message: "Server error get User",
+      message: "Server error. Get User",
+    });
+  }
+});
+
+app.put("/api/user/:id", async (req, res) => {
+  console.log("PUT - ", "/api/user/1");
+  try {
+    const { id } = req.params;
+    const { name, email } = req.body;
+    console.log(req.params);
+    console.log(req.body);
+
+    if (isNaN(id)) {
+      res.status(400).json({
+        succes: false,
+        message: "Invalid id",
+      });
+    }
+    const pool = await poolPromise;
+    const result = await pool
+      .request()
+      .input("id", sql.Int, id)
+      .input("name", sql.VarChar, name)
+      .input("email", sql.VarChar, email)
+      .query(
+        "UPDATE [CURSO_DB].[dbo].[users] SET name=@name, email=@email WHERE id=@id;"
+      );
+    // .query('DELETE FROM [CURSO_DB].[dbo].[users] WHERE id=@id ');
+
+    res.status(200).json(result.rowsAffected);
+  } catch (error) {
+    res.status(500).json({
+      succes: false,
+      message: "Server error. Update user",
+    });
+  }
+});
+
+app.delete("/api/user/:id", async (req, res) => {
+  console.log("DELETE - ", "/api/user/1");
+  try {
+    const { id } = req.params;
+    console.log(req.params);
+
+    if (isNaN(id)) {
+      res.status(400).json({
+        succes: false,
+        message: "Invalid id",
+      });
+    }
+    const pool = await poolPromise;
+    const result = await pool
+      .request()
+      .input("id", sql.Int, id)
+      .query("DELETE FROM [CURSO_DB].[dbo].[users] WHERE id=@id ");
+
+    res.status(200).json(result.rowsAffected);
+  } catch (error) {
+    res.status(500).json({
+      succes: false,
+      message: "Server error. Update user",
     });
   }
 });
